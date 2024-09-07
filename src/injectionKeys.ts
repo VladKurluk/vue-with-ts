74b218f993a4c7d0ad29c9f1608a86e94df51faa
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-09-07 18:04:04 +0300

    Second way for typing provide/inject

--- src/injectionKeys.ts
+++ src/injectionKeys.ts
@@ -1,4 +1,5 @@
 import type { InjectionKey } from "vue";
 import type User from "./types/User";
 
-export const userInjectionKey: InjectionKey<User> = Symbol();
+// export const userInjectionKey: InjectionKey<User> = Symbol();
+export const userInjectionKey = Symbol() as InjectionKey<User>
