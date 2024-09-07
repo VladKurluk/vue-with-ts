e07133f495d6b40718ded3b5431810a3e9e56231
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-09-07 18:11:35 +0300

    Hotfix ts error and typing computed in Options API

--- src/components/DateDisplay.vue
+++ src/components/DateDisplay.vue
@@ -1,12 +1,12 @@
 <script lang="ts">
-import formatRelative from "date-fns/formatRelative";
+import { formatRelative } from "date-fns";
 import { defineComponent } from "vue";
 export default defineComponent({
   props: {
     date: { type: Date, required: true },
   },
   computed: {
-    formatted() {
+    formatted(): string {
       return formatRelative(this.date, Date.now());
     },
   },
