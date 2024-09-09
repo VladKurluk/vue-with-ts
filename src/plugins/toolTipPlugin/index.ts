d935af356ae0e11763c4c17c979c84860b5193d7
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-09-09 22:52:23 +0300

    Add a Global Instance Method with a Vue.js Plugin

--- src/plugins/toolTipPlugin/index.ts
+++ src/plugins/toolTipPlugin/index.ts
@@ -3,11 +3,17 @@ import type { App, Plugin } from "vue";
 import { tooltipOptionsInjectionKey } from "@/injectionKeys";
 import type { TippyOptions } from "./ToolTip.vue";
 import { defu } from "defu";
+import { hideAll } from "tippy.js";
 
 export const toolTipPlugin: Plugin = (app: App, options: TippyOptions): void => {
   options = defu(options, {
     arrow: false
   });
+
+  /**
+   * Hide all visible tooltips.
+   */
+  app.config.globalProperties.$tippyHideAll = hideAll;
 
   app.provide(tooltipOptionsInjectionKey, options);
   const ToolTip = defineAsyncComponent(() => import("./ToolTip.vue"));
