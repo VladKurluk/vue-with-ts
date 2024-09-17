73b62e635e301379e783240f3797b70e9b16fd8e
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-09-17 23:38:23 +0300

    Vue Transition JavaScript Hooks and Greensock

---
+++ src/components/animation/GsapTransition.vue
@@ -0,0 +1,43 @@
+<template>
+  <Transition
+    @before-enter="onBeforeEnter"
+    @enter="onEnter"
+    @leave="onLeave"
+    :css="false"
+  >
+    <slot></slot>
+  </Transition>
+</template>
+
+
+<script lang="ts" setup>
+import gsap from "gsap";
+
+function onBeforeEnter(el: Element): void {
+  gsap.set(el, {
+    scaleX: 0.25,
+    scaleY: 0.25,
+    opacity: 1,
+  });
+}
+function onEnter(el: Element, done: () => void): void {
+  gsap.to(el, {
+    duration: 1,
+    scaleX: 1,
+    scaleY: 1,
+    opacity: 1,
+    ease: "elastic.inOut(2.5, 1)",
+    onComplete: done,
+  });
+}
+function onLeave(el: Element, done: () => void): void {
+  gsap.to(el, {
+    duration: 1,
+    scaleX: 0,
+    scaleY: 0,
+    opacity: 0.2,
+    ease: "elastic.inOut(2.5, 1)",
+    onComplete: done,
+  });
+}
+</script>
--- src/pages/AnimationCourse.vue
+++ src/pages/AnimationCourse.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>Animation Course</h1>
+    <h2 class="text-3xl block text-center mb-2">
+      Animation Course
+    </h2>
     <FadeTransition>
       <span v-if="show">Hello Fade In</span>
     </FadeTransition>
@@ -12,6 +14,16 @@
     <div>
       <button @click="show = !show">Toggle</button>
     </div>
+    <h2 class="text-3xl block text-center mb-2">
+      Animation with GSAP
+    </h2>
+    <GsapTransition>
+      <div v-if="show2" class="circle"></div>
+    </GsapTransition>
+    <br />
+    <div>
+      <button @click="show2 = !show2">Toggle</button>
+    </div>
   </div>
 </template>
 
@@ -20,10 +32,14 @@ import { ref } from "vue";
 
 import FadeTransition from "@/components/animation/FadeTransition.vue";
 import PopTransition from "@/components/animation/PopTransition.vue";
+import GsapTransition from "@/components/animation/GsapTransition.vue";
 
 const show = ref(true);
+const show2 = ref(true);
 </script>
 
-<style scoped>
-
+<style lang="postcss" scoped>
+.circle {
+  @apply w-12 h-12 bg-green-500 rounded-full;
+}
 </style>
