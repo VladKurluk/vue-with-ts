c384f55a0169cab4af3b69ec9185be6314447426
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-09-17 22:50:06 +0300

    Added transition for main menu

--- src/components/layout/menu/mainNavigation.vue
+++ src/components/layout/menu/mainNavigation.vue
@@ -10,48 +10,50 @@
     <span class="sr-only">Main Menu</span>
   </button>
   <!-- drawer component -->
-  <div
-    id="drawer-navigation"
-    :class="[
-      'fixed top-0 left-0 z-40 w-80 h-screen p-4 overflow-y-auto transition-transform bg-white dark:bg-gray-800',
-      isVisibleMenu ? 'translate-none' : '-translate-x-full'
-    ]"
-    tabindex="-1"
-    aria-labelledby="drawer-navigation-label"
-  >
-    <h5
-      id="drawer-navigation-label"
-      class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400"
+  <Transition>
+    <div
+      v-if="isVisibleMenu"
+      id="drawer-navigation"
+      class="fixed top-0 left-0 z-40 w-80 h-screen p-4 overflow-y-auto bg-white dark:bg-gray-800"
+      tabindex="-1"
+      aria-labelledby="drawer-navigation-label"
     >
-      Menu
-    </h5>
-    <button
-      type="button"
-      aria-controls="drawer-navigation"
-      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-      @click="toggleNavMenu"
-    >
-      <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-      </svg>
-      <span class="sr-only">Close menu</span>
-    </button>
-    <div class="py-4 overflow-y-auto">
-      <ul class="space-y-2 font-medium">
-        <navigationItem
-          v-for="(link, index) in mainMenu"
-          :key="index"
-          :menuItem="link"
-          :menuVisibility="isVisibleMenu"
-        />
-      </ul>
+      <h5
+        id="drawer-navigation-label"
+        class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400"
+      >
+        Menu
+      </h5>
+      <button
+        type="button"
+        aria-controls="drawer-navigation"
+        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+        @click="toggleNavMenu"
+      >
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+        </svg>
+        <span class="sr-only">Close menu</span>
+      </button>
+      <div class="py-4 overflow-y-auto">
+        <ul class="space-y-2 font-medium">
+          <navigationItem
+            v-for="(link, index) in mainMenu"
+            :key="index"
+            :menuItem="link"
+            :menuVisibility="isVisibleMenu"
+          />
+        </ul>
+      </div>
     </div>
-  </div>
-  <div
-    v-show="isVisibleMenu"
-    class="bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-30"
-    @click="isVisibleMenu = false"
-  ></div>
+  </Transition>
+  <Transition name="overlay">
+    <div
+      v-show="isVisibleMenu"
+      class="bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-30"
+      @click="isVisibleMenu = false"
+    ></div>
+  </Transition>
 </template>
 
 <script setup lang="ts">
@@ -65,3 +67,38 @@ const toggleNavMenu = (): void => {
   isVisibleMenu.value = !isVisibleMenu.value;
 };
 </script>
+
+<style lang="postcss" scoped>
+.v-enter-active,
+.v-leave-active {
+  @apply transition-transform;
+}
+
+.v-enter-from,
+.v-leave-to {
+  @apply -translate-x-full;
+}
+
+.v-enter-active {
+  @apply delay-150;
+}
+
+.v-leave-from {
+  @apply translate-x-full;
+}
+
+.overlay-enter-active,
+.overlay-leave-active {
+  @apply transition-opacity;
+}
+
+.overlay-enter-from,
+.overlay-leave-to {
+  @apply opacity-0 delay-150;
+}
+
+.overlay-enter-to,
+.overlay-leave-from {
+  @apply opacity-100;
+}
+</style>
