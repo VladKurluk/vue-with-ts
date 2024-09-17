f1feee59d540f4656f088b68b14b1cc53fec5bd9
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-09-17 23:03:08 +0300

    Reusable Vue Transitions

--- src/pages/AnimationCourse.vue
+++ src/pages/AnimationCourse.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
     <h1>Animation Course</h1>
+    <FadeTransition>
+      <span v-if="show">Hello Fade In</span>
+    </FadeTransition>
+    <br />
+    <PopTransition>
+      <span v-if="show">Hello Pop In</span>
+    </PopTransition>
+    <br />
+    <div>
+      <button @click="show = !show">Toggle</button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 
+import FadeTransition from "@/components/animation/FadeTransition.vue";
+import PopTransition from "@/components/animation/PopTransition.vue";
+
+const show = ref(true);
 </script>
 
 <style scoped>
