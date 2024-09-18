26f62903c61e8f13fffc6e10cfb0f696d0771f33
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-09-18 12:43:48 +0300

    List animation with Animate.css

--- src/components/animation/listAnimation.vue
+++ src/components/animation/listAnimation.vue
@@ -1,6 +1,9 @@
 <template>
   <div v-if="pokemon.length">
-    <TransitionGroup tag="ul">
+    <TransitionGroup
+      tag="ul"
+      leave-active-class="animate__animated animate__fadeOutRight absolute"
+    >
       <li v-for="(creature, index) in pokemon" :key="creature.name">
         {{ creature.name }}
         <button
@@ -34,18 +37,8 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=7").then(async (res) => {
 </script>
 
 <style>
-.v-move,
-.v-enter-active,
-.v-leave-active {
+.v-move {
   transition: all 0.5s ease;
-}
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-  transform: translateX(30px);
-}
-.v-leave-active {
-  position: absolute;
 }
 </style>
 
