5a5f37739863e3a9b2a388d5eb832fff7ba6cc77
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-09-20 17:55:20 +0300

    Added UI component for Card

---
+++ src/components/ui/CardComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="card">
+    <slot></slot>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="postcss">
+.card {
+  @apply block max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700;
+}
+
+.card h2 {
+  @apply mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white;
+}
+</style>
