876fd4b3db55edc4f9b038af498962b73d6c8ccc
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-09-20 17:53:15 +0300

    Installation formkit in project. Base configuration.

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import App from "./App.vue";
 import { createToolTipPlugin } from "./plugins/toolTipPlugin";
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 import { MotionPlugin } from '@vueuse/motion';
+import { plugin } from '@formkit/vue';
+import config from '../formkit.config.ts';
 import "@/assets/base.css";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(plugin, config);
 app.use(createToolTipPlugin({
     placement: "top-start",
     delay: 100,
