982a0b2dd359735e7158a912120a0a71f5d91783
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-10-09 21:52:45 +0300

    Created Custom Validation Messages

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,7 +1,16 @@
 import { defaultConfig, DefaultConfigOptions } from '@formkit/vue';
 
 const config: DefaultConfigOptions = {
-  theme: "genesis"
+  theme: "genesis",
+  messages: {
+    en: {
+      validation: {
+        username_is_unique({ args, name, node }) {
+          return `${node.value} is already taken`;
+        },
+      },
+    },
+  },
 };
 
 export default defaultConfig(config);
