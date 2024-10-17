099be33c6b344b0ca30edb278011d5e5a75387b8
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-10-17 14:44:16 +0300

    Charts with Async Data

--- src/components/charts/FirstChart.vue
+++ src/components/charts/FirstChart.vue
@@ -2,6 +2,7 @@
   <CardComponent>
     <h2>First chart</h2>
     <Bar
+      v-if="movies.length"
       :data="chartData"
       :options="{
         plugins: {
@@ -20,7 +21,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import CardComponent from "../ui/CardComponent.vue";
 import { Bar } from "vue-chartjs";
 import {
@@ -43,24 +44,26 @@ ChartJS.register(
   LinearScale
 );
 
-const chartData = ref<ChartData<"bar">>({
-  labels: [
-    "Spider-Man (2002)",
-    "Spider-Man 2 (2004)",
-    "Spider-Man 3 (2007)",
-    "The Amazing Spider-Man (2012)",
-    "The Amazing Spider-Man 2 (2014)",
-    "Spider-Man: Homecoming (2017)",
-    "Spider-Man: Far From Home (2019)",
-    "Spider-Man: No Way Home (2021)",
-    "Spider-Man: Into the Spider-Verse (2018)",
-    "Spider-Man: Across the Spider-Verse (2023)",
-  ],
-  datasets: [
-    {
-      backgroundColor: ["#c82834", "#244771"],
-      data: [7.3, 7.3, 6.2, 6.9, 6.6, 7.4, 7.5, 8.0, 8.4, 8.9],
-    },
-  ],
+type movie = {
+  title: string;
+  rating: number;
+};
+
+const movies = ref<movie[]>([]);
+
+fetch("/api/chart.json")
+  .then(async (res) => (movies.value = await res.json()))
+  .catch((error) => alert(error.message));
+
+const chartData = computed((): ChartData<"bar"> => {
+  return {
+    labels: movies.value.map((movie) => movie.title),
+    datasets: [
+      {
+        backgroundColor: ["#c82834", "#244771"],
+        data: movies.value.map((movie) => movie.rating),
+      },
+    ],
+  };
 });
 </script>
