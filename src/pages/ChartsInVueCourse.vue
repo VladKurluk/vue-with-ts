486aaf0fff3cb7e2c38b5b4851ccec6beae02055
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-10-17 14:33:56 +0300

    First Bar chart

--- src/pages/ChartsInVueCourse.vue
+++ src/pages/ChartsInVueCourse.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    Charts
+    <FirstChart />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import FirstChart from '@/components/charts/FirstChart.vue';
 </script>
 
 <style scoped>
