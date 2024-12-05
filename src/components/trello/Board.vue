c0d7691ff3da9fa9b214d8e36fe7a5a6e17a4743
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-12-05 15:55:16 +0200

    Created minimal featured Drag'n'Drop project with vuedraggable package

---
+++ src/components/trello/Board.vue
@@ -0,0 +1,127 @@
+<script setup lang="ts">
+import { nextTick } from "vue";
+import type { Column, Task } from "@/types/trello";
+import draggable from "vuedraggable";
+import { nanoid } from "nanoid";
+import { useKeyModifier, useLocalStorage } from "@vueuse/core";
+import BoardTask from "./BoardTask.vue";
+import DragHandle from "./DragHandle.vue";
+import NewTask from "./NewTask.vue";
+
+const columns = useLocalStorage<Column[]>(
+  "trelloBoard",
+  [
+    {
+      title: "Backlog",
+      id: nanoid(),
+      tasks: [
+        {
+          title: "Create marketing landing page",
+          createdAt: new Date(),
+          id: nanoid(),
+        },
+        {
+          title: "Develop cool new feature",
+          createdAt: new Date(),
+          id: nanoid(),
+        },
+        { title: "Fix page nav bug", createdAt: new Date(), id: nanoid() },
+      ],
+    },
+    { title: "Selected for Dev", id: nanoid(), tasks: [] },
+    { title: "In Progress", id: nanoid(), tasks: [] },
+    { title: "QA", id: nanoid(), tasks: [] },
+    { title: "Complete", id: nanoid(), tasks: [] },
+  ],
+  {
+    serializer: {
+      read: (value: string) => {
+        return JSON.parse(value).map((column: Column) => {
+          column.tasks = column.tasks.map((task: Task) => {
+            task.createdAt = new Date(task.createdAt);
+            return task;
+          });
+          return column;
+        })
+      },
+      write: (value) => JSON.stringify(value)
+    }
+  }
+);
+const alt = useKeyModifier("Shift");
+
+const createColumn = (): void => {
+  const column: Column = {
+    id: nanoid(),
+    title: "",
+    tasks: [],
+  };
+  columns.value.push(column);
+  nextTick(() => {
+    (
+      document.querySelector(
+        ".column:last-of-type .title-input"
+      ) as HTMLInputElement
+    ).focus();
+  });
+}
+</script>
+
+<template>
+  <div class="flex items-start overflow-x-auto gap-4">
+    <draggable
+      v-model="columns"
+      group="columns"
+      :animation="100"
+      handle=".drag-handle"
+      item-key="id"
+      class="flex gap-4 items-start"
+    >
+      <template #item="{ element: column }: { element: Column }">
+        <div class="column bg-gray-200 dark:bg-slate-800 p-5 rounded min-w-[250px]">
+          <header class="font-bold mb-4">
+            <DragHandle />
+            <input
+              class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+              @keyup.enter="($event.target as HTMLInputElement).blur()"
+              @keydown.backspace="
+                column.title === ''
+                  ? (columns = columns.filter((c) => c.id !== column.id))
+                  : null
+              "
+              type="text"
+              v-model="column.title"
+            />
+          </header>
+          <draggable
+            v-model="column.tasks"
+            :group="{ name: 'tasks', pull: alt ? 'clone' : true }"
+            :animation="150"
+            item-key="id"
+          >
+            <template #item="{ element: task }: { element: Task }">
+              <div>
+                <BoardTask
+                  :task="task"
+                  @delete="
+                    column.tasks = column.tasks.filter((t) => t.id !== $event)
+                  "
+                />
+              </div>
+            </template>
+          </draggable>
+
+          <footer>
+            <NewTask @add="column.tasks.push($event)" />
+          </footer>
+        </div>
+      </template>
+    </draggable>
+    <button
+      class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50"
+      @click="createColumn"
+    >
+      + Add Another Column
+    </button>
+  </div>
+</template>
--- src/pages/TrelloCloneWithVue.vue
+++ src/pages/TrelloCloneWithVue.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    Trello clone
+  <div class="min-h-screen bg-teal-600 overlow-auto">
+    <h1 class="text-4xl text-white flex items-center mb-10">
+      <img
+        width="200"
+        class="mr-3"
+        src="https://vueschool.io/img/logo/vueschool_logo_multicolor_negative.svg"
+        alt="Vue School"
+      />
+      Trello Board
+    </h1>
+    <Board />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import Board from '@/components/trello/Board.vue';
 </script>
